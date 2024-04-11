f7f3f3259efb065a7d22be5377e9cc1549fe9327
fullm <matoma1_@naver.com>
2024-04-11 11:58:27 +0900

    Initial commit

---
+++ src/main/java/controller/CalcController.java
@@ -0,0 +1,36 @@
+package controller;
+
+import service.MyService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/calc")
+public class CalcController extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //클라이언트 폼에서 넘어온 파라이터를 얻기 (가져오기) req를 사용
+        int su1 = Integer.parseInt(req.getParameter("su1")); //숫자가 아니라 문자열로 넘어감 "50" -> 숫자 50으로 바꾸기
+        int su2 = Integer.parseInt(req.getParameter("su2"));
+        int sum = MyService.hap(su1, su2);
+        resp.setContentType("text/html;charset=UTF-8"); // MINE TYPE 보내는 데이터 타입. 한글이고 텍스트이고 html이다.
+        PrintWriter out = resp.getWriter(); //응답스트림
+        out.println("<html>"); //출력내용
+        out.println("<body>"); //뷰를 jsp로 뻬자.
+        out.println("<table border ='1'>");
+        out.println("<tr>");
+        out.println("<td>총합</td>");
+        out.println("<td>");
+        out.println(sum);
+        out.println("</td>");
+        out.println("</tr>");
+        out.println("<table>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
---
+++ src/main/java/controller/HelloServlet.java
@@ -0,0 +1,32 @@
+package controller;
+
+import service.MyService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//javase => jdk 설치시 탑재(API List,Map, Set)
+// javaee ->API 필요하다
+//httpservlet - web용 api
+//http://localhost:8081/weberp/hello
+@WebServlet("/hello") //servlet Mapping. hello라는 요청이 왔을 때 맵핑해줌
+public class HelloServlet extends HttpServlet { //Servlet : 서버에서 100% 자바로 만들어진 웹프로그래밍
+
+    @Override       //req = 요청객체, resp = 응답 객체
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //1-10까지의 합 =? 응답 resp이용. 여기 안에 클라이언트 정보가 잇다.
+        //응답을 하려면 다른쪽의 ip addr,port number를 알아야 한다.
+        // 클라이언트에 응답할 출력 스트림 (빨대를 만들어야 한다)
+        //응답 빨대 PrintWriter. out을 사용해 데이터를 내보내자
+
+        //1-10의 합을 구하는 방법 1. 직접구한다. 2. 별도의 클래스에서 처리
+        int hap = MyService.hap(); //객체 생성. static 이니까 new 필요 없음
+
+        //클라이언트에게 어떻게 요청?
+    }
+}
