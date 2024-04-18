a37bd7bc012136fc65e334e475c9c2e15bd230f2
fullm <matoma1_@naver.com>
2024-04-18 14:39:08 +0900

    BookDAO 생성

---
+++ src/main/java/repository/BookDAO.java
@@ -0,0 +1,10 @@
+package repository;
+
+//JDBC -> MyBatis -> Spring My Batis -> Hibernate(OPM) -> JPA
+//JDBC -> Java + SQL = 유지보수 어려움. 생산성 떨어짐
+//MyBatis -> SQL과 JAVA를 분리, 매핑 시켜준다.
+public class BookDAO {
+
+}
+
+
